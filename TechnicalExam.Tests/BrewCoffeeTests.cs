eef46148d222d7fea2535dbfd2c87853dc51d17b
Jiriku <officialjerichom@gmail.com>
2026-06-03 12:34:27 +0800

    Add project files.

---
+++ TechnicalExam.Tests/BrewCoffeeTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Technical_Examination.WebApi.Controllers;
+using Technical_Examination.WebApi.DataContext;
+
+namespace TechnicalExam.Tests;
+
+public class BrewCoffeeTests
+{
+
+    [Theory]
+    [MemberData(nameof(BrewCasesData))]
+    public void Return503_After5Endpoints(string date, int status_code)
+    {
+        var new_coffee = new BrewCoffee
+        {
+            Prepared = DateTime.Parse(date)
+        };
+        var controller = new BrewCoffeeController();
+        var result = controller.Get(new_coffee);
+
+        if (result.Result is ObjectResult objresult)
+        {
+            Assert.Equal(status_code, objresult.StatusCode);
+
+            if (status_code == 200)
+            {
+                var coffee = objresult.Value as BrewCoffee;
+                Assert.NotNull(coffee);
+                Assert.Equal("Your piping hot coffee is ready", coffee.Message);
+                Assert.Equal(DateTime.Parse(date), coffee.Prepared);
+            }
+            else if (status_code == 503)
+            {
+                Assert.Equal("503 Service Unavailable.", objresult.Value?.ToString());
+            }
+        }
+        else if (result.Result is StatusCodeResult statusResult)
+        {
+            Assert.Equal(status_code, statusResult.StatusCode);
+        }
+    }
+
+    public static IEnumerable<object[]> BrewCasesData()
+    {
+        yield return new object[] { "2025-12-17T12:46:00Z", 200};
+        yield return new object[] { "2021-11-10T20:43:00+08:00", 200 };
+        yield return new object[] { "2022-02-21T14:33:00+02:00", 200 };
+        yield return new object[] { "2024-07-23T08:36:00-04:00", 200 };
+        yield return new object[] { "2026-06-06T12:06:00Z", 503};
+    }
+
+}
---
+++ Technical_Examination.WebApi/Controllers/BrewCoffeeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.VisualBasic;
+using Technical_Examination.WebApi.DataContext;
+
+namespace Technical_Examination.WebApi.Controllers;
+
+[ApiController]
+[Route("brew-coffee")]
+public class BrewCoffeeController : ControllerBase
+{
+    public static int _counter = 1;
+
+    [HttpGet]
+    public ActionResult<BrewCoffee> Get(BrewCoffee? cofee_data)
+    {
+        var current_date = DateTime.Now;
+        var current_month = current_date.Month;
+        var current_day = current_date.Day;
+
+        if (_counter < 5)
+        {
+            Interlocked.Increment(ref _counter);
+            if (DateTime.Equals(current_month, 4) && DateTime.Equals(current_day, 1))
+                return StatusCode(418, "418 I’m a teapot");
+
+            var coffee = new BrewCoffee()
+            {
+                Message = "Your piping hot coffee is ready",
+                Prepared = cofee_data.Prepared ?? DateTime.Parse(DateTime.Now.ToString("O"))
+            };
+            return Ok(coffee);
+        }
+        else
+        {
+            return StatusCode(503, "503 Service Unavailable.");
+        }
+    }
+    public void ResetCounter() => _counter = 0;
+}
